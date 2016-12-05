8f0b4956cd81359113278015b25bc1d476384056
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-12-05 17:16:46 +0000

    Generate smaller images

--- app/controllers/primitive_controller.rb
+++ app/controllers/primitive_controller.rb
@@ -5,7 +5,7 @@ class PrimitiveController < ApplicationController
   def create
     in_filename = params[:picture].tempfile.path
     out_filename = in_filename.gsub("/tmp/", "/tmp/out-")
-    `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50`
+    `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50 -s 400`
     render text: File.read(out_filename), content_type: "image/jpeg"
   end
 
@@ -13,7 +13,7 @@ class PrimitiveController < ApplicationController
     in_filename = "/tmp/#{Digest::SHA1.hexdigest(params[:url])}.jpg"
     out_filename = in_filename.gsub("/tmp/", "/tmp/out-")
     IO.copy_stream(open(params[:url]), in_filename)
-    `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50`
+    `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50 -s 400`
     expires_in 2.hours
     render text: File.read(out_filename), content_type: "image/jpeg"
   end
