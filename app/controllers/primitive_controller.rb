d82694a803e4b06bbdefc967a06c8f05324fdfb4
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-12-05 17:42:07 +0000

    Add simple result cache using cloudinary

--- app/controllers/primitive_controller.rb
+++ app/controllers/primitive_controller.rb
@@ -10,11 +10,25 @@ class PrimitiveController < ApplicationController
   end
 
   def create_from_url
-    in_filename = "/tmp/#{Digest::SHA1.hexdigest(params[:url])}.jpg"
-    out_filename = in_filename.gsub("/tmp/", "/tmp/out-")
-    IO.copy_stream(open(params[:url]), in_filename)
-    `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50 -s 400`
-    expires_in 2.hours
-    render text: File.read(out_filename), content_type: "image/jpeg"
+    expires_in 30.minutes
+
+    if image = Image.find_by_seed(params[:url])
+      render text: open(image.result).read, content_type: "image/jpeg"
+    else
+      in_filename = "/tmp/#{Digest::SHA1.hexdigest(params[:url])}.jpg"
+      out_filename = in_filename.gsub("/tmp/", "/tmp/out-")
+      IO.copy_stream(open(params[:url]), in_filename)
+      `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50 -s 400`
+
+      url = Cloudinary::Uploader.upload(out_filename,
+                                        public_id: "shuffled-#{Digest::SHA1.hexdigest(params[:url])}.jpg",
+                                        cloud_name: "charlieegan3",
+                                        api_key: ENV['CLOUDINARY_KEY'],
+                                        api_secret: ENV['CLOUDINARY_SECRET'])['url']
+
+      Image.create(seed: params[:url], result: url)
+
+      render text: File.read(out_filename), content_type: "image/jpeg"
+    end
   end
 end
