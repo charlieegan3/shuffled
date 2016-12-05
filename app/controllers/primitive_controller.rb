919c5252868ca6cdfd8c7fe45fc800497352a5bf
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-12-05 11:58:32 +0000

    Add search to create for term

--- app/controllers/primitive_controller.rb
+++ app/controllers/primitive_controller.rb
@@ -1,11 +1,18 @@
 class PrimitiveController < ApplicationController
   def index
-    # render text: `./bin/primitive`
   end
 
   def create
     in_filename = params[:picture].tempfile.path
     out_filename = in_filename.gsub("/tmp/", "/tmp/out-")
+    `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50`
+    render text: File.read(out_filename), content_type: "image/jpeg"
+  end
+
+  def create_from_url
+    in_filename = "/tmp/#{Digest::SHA1.hexdigest(params[:url])}.jpg"
+    out_filename = in_filename.gsub("/tmp/", "/tmp/out-")
+    IO.copy_stream(open(params[:url]), in_filename)
     `./bin/primitive -i #{in_filename} -o #{out_filename} -n 50`
     render text: File.read(out_filename), content_type: "image/jpeg"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'primitive', to: 'primitive#index'
+  get 'primitive/from_url', to: 'primitive#create_from_url'
   post 'primitive', to: 'primitive#create'
 
   get 'image_search', to: 'image_search#index'
-  post 'image_search', to: 'image_search#search'
+  get 'image_search/results', to: 'image_search#search'
 end
